9e4bfc97a43f4a8dc5589a880c79834d55385236
Kathleen Leung <kathleenleungky@gmail.com>
2019-09-13 14:28:02 -0700

    added functionality in server.js for routing and listening

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Importing routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
