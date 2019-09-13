1ec95ea71fe126972cf3672b283e1673191a130a
Kathleen Leung <kathleenleungky@gmail.com>
2019-09-13 14:59:45 -0700

    added data list in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+// DATA
+var friends = [
+    {
+        "name":"Scarlett",
+        "photo":"https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name":"John",
+        "photo":"https://media.nesta.org.uk/images/john_davies.2e16d0ba.fill-600x600.jpg",
+        "scores":[4,2,3,4,5,3,1,2,5,1]
+    },
+    {
+        "name":"Kelsey",
+        "photo":"https://cdn.shopify.com/s/files/1/0003/4580/0755/articles/Screenshot_2018-08-13_15.36.30_1400x_d132a293-f2ff-4c77-80b0-bdc39610f46e.jpg?v=1557361908",
+        "scores":[3,1,4,2,5,4,2,3,4,5]
+    },
+    {
+        "name":"Louis",
+        "photo":"https://i.pinimg.com/originals/3e/90/04/3e9004df7724847e81162dace6922a6d.jpg",
+        "scores":[1,4,4,2,5,1,3,5,4,3]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
