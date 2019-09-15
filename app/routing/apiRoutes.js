66b3ab467eda1ea075d361d1bd423e2d9ac6cf6f
Kathleen Leung <kathleenleungky@gmail.com>
2019-09-14 20:46:33 -0700

    fixed styling and finished apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,5 +11,34 @@ module.exports = function(app){
     app.post("/api/friends", function(req, res){
         var newUser = req.body;
         console.log(newUser);
+
+        // Differences between Friend & User
+        var differences = [];
+
+        friendsArr.forEach(function(friend){
+            // Total difference
+            var totalDifference = 0;
+
+            for (var i = 0; i < newUser.scores.length; i++){
+                var difference = friend.scores[i] - parseInt(newUser.scores[i]);
+                totalDifference += Math.abs(difference);
+                console.log(totalDifference);
+            }
+            differences.push(totalDifference);
+        });
+        // Sum of differences between current user and available Friends in Queue: 
+        console.log(differences);
+        
+        var leastDiff = Math.min.apply(null, differences);
+
+        var bestMatch;
+
+        for(var i = 0; i < differences.length; i++){
+            if(differences[i] === leastDiff){
+                bestMatch = friendsArr[i];
+            }
+        }
+        res.json(bestMatch);
+        friendsArr.push(newUser);
     });
 }
